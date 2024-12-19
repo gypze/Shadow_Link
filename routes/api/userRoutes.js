35e64fd0228b42f3631e3c738503ea5f389001e4
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-19 08:22:53 -0600

    created user and thought routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,17 @@
 
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add more routes here: POST, PUT, DELETE
+
+module.exports = router;
